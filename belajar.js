ae7aef382995b4e3f5b9ec9d8a6ba46b4058fbef
Nadzimfawwaz321 <e42241971@student.polije.ac.id>
2025-09-10 14:16:22 +0700

    latihan_bisdig

---
+++ belajar.js
@@ -0,0 +1,9 @@
+alert("Halo Valdo WAZZ HEREEE!");
+
+let konfirmasi = confirm("Mau belajar JavaScript?");
+if (konfirmasi) {
+  let umur = prompt("Berapa umurmu?");
+  alert("Wah, umur kamu " + umur + " tahun!");
+} else {
+  alert("Tidak apa-apa, mungkin lain kali 😄");
+}
